3350ee6a181fd7a8b04438427aafc9b4088bd93c
Santiago Fleitas <sansfleitas@gmail.com>
2023-01-18 20:15:39 -0300

    Add React Fragments to App component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 function App() {
-  return <h1>Componente App</h1>;
+  return (
+    <>
+      <h1>Componente App</h1>
+      <h2>Subtítulo</h2>
+    </>
+  );
 }
 
 export default App;
