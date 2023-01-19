a33ae0a619eda12c4e8e743904323261fe9ef9dd
Santiago Fleitas <sansfleitas@gmail.com>
2023-01-19 17:58:13 -0300

    Add Inline CSS

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
+
 import Button from './Button';
 
 import { ThemeContext } from './ThemeContext';
@@ -8,13 +9,19 @@ export default function Header(props) {
   const { onToggleTheme } = useContext(ThemeContext);
 
   return (
-    <>
+    <div
+      style={{
+        background: '#ccc',
+        fontFamily: 'sans-serif',
+        marginBottom: '24rem',
+      }}
+    >
       <h1>{props.title}</h1>
       <Button onClick={onToggleTheme}>
         Mudar tema
       </Button>
       {props.children}
-    </>
+    </div>
   );
 }
 
