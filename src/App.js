42edc8a4f50c6fc26c4d4b35f14c83669c193835
Santiago Fleitas <sansfleitas@gmail.com>
2023-01-19 18:50:53 -0300

    Add dynamic classes

--- src/App.js
+++ src/App.js
@@ -10,10 +10,10 @@ import { ThemeProvider } from './ThemeContext';
 // Props -> Properties
 function App() {
   const [posts, setPosts] = useState([
-    { id: Math.random(), title: 'Title#01', subtitle: 'Sub#01', likes: 20, read: false },
-    { id: Math.random(), title: 'Title#02', subtitle: 'Sub#02', likes: 10, read: true },
-    { id: Math.random(), title: 'Title#03', subtitle: 'Sub#03', likes: 50, read: false },
-    { id: Math.random(), title: 'Title#04', subtitle: 'Sub#03', likes: 50, read: true },
+    { id: Math.random(), title: 'Title#01', subtitle: 'Sub#01', likes: 20, read: false, removed: false },
+    { id: Math.random(), title: 'Title#02', subtitle: 'Sub#02', likes: 10, read: true, removed: false },
+    { id: Math.random(), title: 'Title#03', subtitle: 'Sub#03', likes: 50, read: false, removed: false },
+    { id: Math.random(), title: 'Title#04', subtitle: 'Sub#03', likes: 50, read: true, removed: false },
   ]);
 
   function handleRefresh(e) {
@@ -27,7 +27,9 @@ function App() {
   }
 
   function handleRemovePost(postId) {
-    setPosts(prevState => prevState.filter(post => post.id !== postId));
+    setPosts(prevState => (
+      prevState.map(post => post.id === postId ? { ...post, removed: !post.removed } : post)
+    ));
   }
 
   return (
--- src/Post.js
+++ src/Post.js
@@ -3,25 +3,28 @@ import PropTypes from 'prop-types';
 
 import PostHeader from './PostHeader';
 
+import styles from './Post.scss';
+
 export default function Post(props) {
   return (
-    <>
-      <article>
-        <PostHeader
-          onRemove={props.onRemove}
-          post={{
-            id: props.post.id,
-            title: props.post.title,
-            read: props.post.read,
-          }}
-        />
-        <br />
-        <small>{props.post.subtitle}</small>
-        <br />
-        Media: {props.post.likes / 2}
-      </article>
+    <article
+      className={
+        props.post.removed ? styles.postDeleted : styles.post
+      }
+    >
+      <PostHeader
+        onRemove={props.onRemove}
+        post={{
+          id: props.post.id,
+          title: props.post.title,
+          read: props.post.read,
+        }}
+      />
       <br />
-    </>
+      <small>{props.post.subtitle}</small>
+      <br />
+      Media: {props.post.likes / 2}
+    </article>
   );
 }
 
@@ -32,6 +35,7 @@ Post.propTypes = {
     title: PropTypes.string.isRequired,
     subtitle: PropTypes.string.isRequired,
     likes: PropTypes.number.isRequired,
-    read: PropTypes.bool.isRequired
+    read: PropTypes.bool.isRequired,
+    removed: PropTypes.bool.isRequired
   }).isRequired,
 };
