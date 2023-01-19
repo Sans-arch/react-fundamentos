4cbdcf822f6a6f0333b5251ff135c42dfa9fc343
Santiago Fleitas <sansfleitas@gmail.com>
2023-01-18 22:02:04 -0300

    Add JS expressions in JSX code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 
+const category = 'Posts da semana';
+
 function App() {
   return (
     <>
-      <h1>Componente App</h1>
-      <h2>Subtítulo</h2>
+      <h1>JStack's Blog {Math.random()}</h1>
+      <h2>{category}</h2>
     </>
   );
 }
