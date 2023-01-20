343f114486a54fa7e48e4c07ae8f1608f281b815
Santiago Fleitas <sansfleitas@gmail.com>
2023-01-20 10:51:07 -0300

    Add useTheme() to get the theme object with all properties

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,15 +1,28 @@
 import React from 'react';
+import { useTheme } from 'styled-components';
 
 import Header from '../Header';
 import PostsList from '../PostsList';
 import Footer from '../Footer';
 
 export default function Layout({ onToggleTheme, selectedTheme }) {
+  // Acessando os valores do tema através do useTheme() que retorna o objeto
+  // com os dados do tema
+  const theme = useTheme();
+
   return (
     <>
       <Header onToggleTheme={onToggleTheme} selectedTheme={selectedTheme} />
       <PostsList />
       <Footer onToggleTheme={onToggleTheme} selectedTheme={selectedTheme} />
+
+      <div style={{
+        marginTop: 24,
+        backgroundColor: theme.footerBackgroundColor,
+        padding: 24,
+      }}>
+        Oi tudo bem com você?
+      </div>
     </>
   );
 }
