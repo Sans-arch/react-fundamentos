01efb8c8d52bd1badeef775994788be76a5695df
Santiago Fleitas <sansfleitas@gmail.com>
2023-01-19 21:13:34 -0300

    Add function css to work in a different way with props in styled components

--- src/components/Post/styles.js
+++ src/components/Post/styles.js
@@ -1,12 +1,17 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 // Componentes criados com styled-components são componentes React normais,
 // então podem receber props normalmente.
 // Em styled components, para usar as props, executamos funções dentro, e o parametro será as props.
 export const Container = styled.article`
   margin-bottom: 24px;
-  opacity: ${props => props.removed ? 0.5 : 1};
-  color: ${props => props.removed ? '#f00' : '#fff'};
+  /* opacity: ${props => props.removed ? 0.5 : 1};
+  color: ${props => props.removed ? '#f00' : '#fff'}; */
+
+  ${props => css`
+    color: ${props.removed ? '#f00' : '#fff'};
+    opacity: ${props.removed ? 0.5 : 1};
+  `}
 `;
 
 export const Subtitle = styled.small`
