422fb2b48f157f3156aab4e5252609e56df929b0
Santiago Fleitas <sansfleitas@gmail.com>
2023-01-20 19:09:00 -0300

    Refactor PostsList functional component to class component

--- src/components/PostsList/index.js
+++ src/components/PostsList/index.js
@@ -5,16 +5,18 @@ import Post from './Post';
 
 import posts from './posts';
 
-export default function PostsList() {
-  return (
-    <Container>
-      {posts.map((post) => (
-        <Post
-          key={post.id}
-          title={post.title}
-          description={post.description}
-        />
-      ))}
-    </Container>
-  );
+export default class PostList extends React.Component {
+  render() {
+    return (
+      <Container>
+        {posts.map((post) => (
+          <Post
+            key={post.id}
+            title={post.title}
+            description={post.description}
+          />
+        ))}
+      </Container>
+    );
+  }
 }
