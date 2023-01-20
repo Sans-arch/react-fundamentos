c444594ee7a6eb053b2a8a6b174a8ea151568ffe
Santiago Fleitas <sansfleitas@gmail.com>
2023-01-19 21:09:04 -0300

    Add props and work with props in Styled components

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -2,17 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import PostHeader from './PostHeader';
-import styles from './Post.scss';
 
-import { Subtitle, Rate } from './styles';
+import { Container, Subtitle, Rate } from './styles';
 
 export default function Post(props) {
   return (
-    <article
-      className={
-        props.post.removed ? styles.postDeleted : styles.post
-      }
-    >
+    <Container removed={props.post.removed}>
       <PostHeader
         onRemove={props.onRemove}
         post={{
@@ -21,10 +16,9 @@ export default function Post(props) {
           read: props.post.read,
         }}
       />
-
       <Subtitle>{props.post.subtitle}</Subtitle>
       <Rate>Media: {props.post.likes / 2}</Rate>
-    </article>
+    </Container>
   );
 }
 
--- src/components/Post/styles.js
+++ src/components/Post/styles.js
@@ -1,5 +1,14 @@
 import styled from "styled-components";
 
+// Componentes criados com styled-components são componentes React normais,
+// então podem receber props normalmente.
+// Em styled components, para usar as props, executamos funções dentro, e o parametro será as props.
+export const Container = styled.article`
+  margin-bottom: 24px;
+  opacity: ${props => props.removed ? 0.5 : 1};
+  color: ${props => props.removed ? '#f00' : '#fff'};
+`;
+
 export const Subtitle = styled.small`
   display: block;
 `;
